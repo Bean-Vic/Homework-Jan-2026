3e93d25b8daa8760aa60055013a75b8685bbcde2
bloomii <156985656+bloomii@users.noreply.github.com>
2026-01-22 22:27:06 -0600

    Update 2026-01-21-LC-98.js

--- HW5/WU_YUTONG/2026-01-21-LC-98.js
+++ HW5/WU_YUTONG/2026-01-21-LC-98.js
@@ -1,1 +1,25 @@
+var isValidBST = function(root) {
+    let prevVal = -Infinity;
 
+    function dfs(node) {
+        if (node === null) {
+            return true;
+        }
+
+        if (!dfs(node.left)) {
+            return false;
+        }
+
+        if (node.val <= prevVal) {
+            return false;
+        }
+        prevVal = node.val;
+
+        if (!dfs(node.right)) {
+            return false;
+        }
+        return true;
+    }
+
+    return dfs(root);
+};
