938c4e3f7f192a3f7c3aeb2146e9899941155c34
bloomii <156985656+bloomii@users.noreply.github.com>
2026-01-24 13:13:16 -0600

    Update 2026-01-24-LC-153.js

--- HW6/WU_YUTONG/2026-01-24-LC-153.js
+++ HW6/WU_YUTONG/2026-01-24-LC-153.js
@@ -1,1 +1,16 @@
+var findMin = function(nums) {
+    const last = nums[nums.length - 1];
+    let l = 0, r = nums.length - 1;
 
+    while (l < r) {
+        const mid = Math.floor((l + r) / 2);
+
+        if (nums[mid] <= last) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    return nums[l];
+};
