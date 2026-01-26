27694cade868555b19a18604b41e689a9a2e84d4
bloomii <156985656+bloomii@users.noreply.github.com>
2026-01-26 01:38:11 -0600

    Update App.jsx

--- HW6/WU_YUTONG/App.jsx
+++ HW6/WU_YUTONG/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Counter from "./counter";
+import { Shoppingcart } from "./Shoppingcart";
 
 export default function App() {
   return (
     <div>
       <Counter initialValue={1} />
+
+      <hr />
+
+      <Shoppingcart />
     </div>
   );
 }
