d9005a3d8dc8824bdea2bab1da8c2bb00df20e65
bloomii <156985656+bloomii@users.noreply.github.com>
2026-02-01 23:01:08 -0600

    Update counterlocal.js

--- HW6/WU_YUTONG/counterlocal.js
+++ HW6/WU_YUTONG/counterlocal.js
@@ -1,1 +1,32 @@
+import React from "react";
 
+export function counterlocal(component, key) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+
+      const saved = localStorage.getItem(key);
+      const savedNumber = Number(saved);
+
+      if (saved !== null) {
+        this.initialValue = savedNumber;
+      } else {
+        this.initialValue = props.initialValue;
+      }
+    }
+
+    handleSaveValue = (value) => {
+      localStorage.setItem(key, value);
+    };
+
+    render() {
+      return (
+        <component
+          {...this.props}
+          initialValue={this.initialValue}
+          saveValue={this.handleSaveValue}
+        />
+      );
+    }
+  };
+}
