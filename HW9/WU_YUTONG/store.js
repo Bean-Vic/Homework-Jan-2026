b55224c7826420c0d5aa89630c35f49bf1414877
bloomii <156985656+bloomii@users.noreply.github.com>
2026-02-01 20:56:12 -0600

    Update store.js

--- HW9/WU_YUTONG/store.js
+++ HW9/WU_YUTONG/store.js
@@ -1,1 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterReducer";
 
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
