258e50dcb78b64faf782a320c4b6b8019c84e454
Sherry <leixue888@outlook.com>
2026-02-01 16:45:01 -0800

    Uploaded HW9

---
+++ HW9/sherry/2026-2-1-LC-70.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 2) return n;
+  let prev2 = 1;
+  let prev1 = 2;
+  for (let i = 3; i <= n; i++) {
+    const cur = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = cur;
+  }
+  return prev1;
+};
